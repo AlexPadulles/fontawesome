ed66fc8e2f9f171699179eb9ca7b97f9adc8ad76
AlexPadulles <87446258+AlexPadulles@users.noreply.github.com>
2023-08-16 09:52:50 +0200

    Update app.js
    Mejora

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ $(document).ready(function () {
         let v = JSON.stringify(response, null, 2);
         // var jsonObject = JSON.parse(v);
         console.log('Respuesta JSON recibida y procesada:', v);
+        span.text('Duplicados eliminados, abre la consola y copia el JSON ');
       },
       error: function (error) {
         console.error('Error:', error);
